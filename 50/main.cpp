78110e2a5b87009b37f7739f3b8d54316c84d98a
jianiw <wjn9424@163.com>
2021-09-14 10:31:42 -0700

    gg day 10

--- 50/main.cpp
+++ 50/main.cpp
@@ -18,6 +18,29 @@ public:
     }
 };
 
+class Solution {
+public:
+    double fastPower(double x, long long n){
+        if(n == 0){
+            return 1.0;
+        }
+        double half = fastPower(x, n/2);
+        if(n % 2 == 0){
+            return half * half;
+        }else{
+            return half * half * x;
+        }
+    }
+    double myPow(double x, int n){
+        long long N = n;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+        return fastPower(x, N);
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
